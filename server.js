c4efa5cb650bb288afaa8295bd91b87a5a7e8686
Rekha1912 <rekha.0019s@gmail.com>
2020-11-02 19:56:53 -0600

    added db models

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const router = express.Router();
+const app = express();
+
+connectDB();
+app.get('/', (req,res) => res.send('API Running')); 
+
+//Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/post',require('./routes/api/post'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
+    console.log('Express app listening on port ' + PORT);
+});
