fa1431b47cde2cea044f32092683ce2d3049bcbd
Rekha1912 <rekha.0019s@gmail.com>
2020-11-06 13:34:55 -0600

    Installed react app

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/api/users',require('./routes/api/users'));
 app.use('/api/quest',require('./routes/api/quest'));
 app.use('/api/auth',require('./routes/api/auth'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, function(){
     console.log('Express app listening on port ' + PORT);
 });
