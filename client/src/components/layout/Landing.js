0521c545ab2b134c7537ccc72f7399e5d3948b5c
Rekha1912 <rekha.0019s@gmail.com>
2020-11-20 20:51:37 +0530

    added landing.js

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Landing= () => {
+    return (
+        <div className = "landing">
+
+                <h1> WANDERLUST </h1>
+                <h3> Travel related questions and their answers for Top Five countries </h3>
+                <div className = "buttons">
+                    <a href = "register.html" class= "btn btn-primary"> Sign Up </a>
+                    <a href = "login.html" class= "btn btn-primary"> Login </a>
+                </div>
+            
+
+            
+        </div>
+    )
+}
+
+export default Landing;
