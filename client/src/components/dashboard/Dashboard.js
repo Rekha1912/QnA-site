6d1978f0f1c4e13a5c6503e78eb25287d474c3d1
Rekha1912 <rekha.0019s@gmail.com>
2021-05-19 20:16:06 -0500

    Dashboard page

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+/*
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
+*/
+
+const Dashboard = props => {
+    return <div> Dashboard </div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
